2e7c2b0562a8f7e79ea2282d93512087d270e910
SGauthier2Pro <Sylvain.Gauthier2Pro@gmail.com>
2022-02-21 18:28:41 +0100

    modification du main, gestion de l'appel de programme avec ou sans parametre, ajout des fonctions test_url, get_choice_menu_UI, fill_result_file, get_result_filename, get_book_link_in_page, get_categories_list, get_category_link, en construction de la fonction get_categories_link_list en commentaire dans le script

--- main.py
+++ main.py
@@ -82,67 +82,227 @@ def get_book_availability(page_content):
     return availability_number
 
 
-def get_categories_link_list(page_content):
+def get_category_link(url_link, category_name):
+    # Fonction de recuperation de la liste des liens de chaque categorie
+    page = requests.get(url_link)
+    category_soup_content = BeautifulSoup(page.content, 'html.parser')
+    category_link = ""
+    ul_nav_list = category_soup_content.find_all("ul", class_="nav nav-list")
+    lis_categories = ul_nav_list[0].find_all("li")
+    for li in lis_categories:
+        a_label = li.find("a")
+        if re.sub("\n|\s","",a_label.string) == category_name:
+            link = a_label["href"]
+            category_link = link.replace("../", "https://books.toscrape.com/")
+    return category_link
+
+
+def get_categories_list(url_link):
+    # Fonction de recuperation de la liste des liens de chaque categorie
+    page = requests.get(url_link)
+    categories_soup_content = BeautifulSoup(page.content, 'html.parser')
+
+    categories_list = []
+
+    ul_nav_list = categories_soup_content.find_all("ul", class_="nav nav-list")
+    lis_categories = ul_nav_list[0].find_all("li")
+    for li in lis_categories:
+        a_label = li.find("a")
+        string_category = re.sub("\n|\s", "", str(a_label.string))
+        if string_category != 'Books' and string_category != 'None':
+            categories_list.append(string_category)
+    return categories_list
+
+"""
+def get_categories_link_list(url_link):
     # Fonction de recuperation de la liste des liens de chaque categorie
     categories_link_list = []
     ul_nav_list = page_content.find_all("ul", class_="nav nav-list")
-    lis_categories = ul_nav_list.find_all("li")
+    lis_categories = ul_nav_list[0].find_all("li")
     for li in lis_categories:
         a_balise = li.find("a")
         link = a_balise["href"]
-        categories_link_list.append("https://books.toscrape.com" + link)
+        categories_link_list.append("https://books.toscrape.com/" + link)
     return categories_link_list
+"""
 
+def get_book_link_in_page(url_link):
+    # Fonction renvoyant la liste des url de livre d'un page
 
-def get_data_book(url_link, page_content):
+    # declaration de la liste a retourner
+    page_book_link_list = []
+
+    page = requests.get(url_link)
+    page_soup_content = BeautifulSoup(page.content, 'html.parser')
+    articles_container = page_soup_content.find_all("article")
+    for article in articles_container:
+        h3_balise = article.find("h3")
+        a_link = h3_balise.find("a")
+        link = a_link["href"]
+        page_book_link_list.append(link.replace("../../..", "https://books.toscrape.com/catalogue"))
+    return page_book_link_list
+
+def get_data_book(url_link):
     # Fonction de recuperation des information sur un livre
-    book_table = get_book_page_table(page_content)
+    page = requests.get(url_link)
+    book_soup_content = BeautifulSoup(page.content, 'html.parser')
+    book_table = get_book_page_table(book_soup_content)
     book_data_list = [url_link, book_table["UPC"],
-                     get_book_title(page_content),
+                     get_book_title(book_soup_content),
                      book_table["Price (incl. tax)"],
                      book_table["Price (excl. tax)"],
-                     get_book_availability(page_content),
-                     get_book_description(page_content),
-                     get_book_category(page_content),
-                     get_book_review_rating(page_content),
-                     get_image_url(page_content)]
+                     get_book_availability(book_soup_content),
+                     get_book_description(book_soup_content),
+                     get_book_category(book_soup_content),
+                     get_book_review_rating(book_soup_content),
+                     get_image_url(book_soup_content)]
     return book_data_list
 
+
+def get_result_filename():
+    # Fonction retournant le fichier de resultat
+    choice_result_filename = input(
+        "Entrer le nom du fichier de resultat sans extension (si laissé a vide le nom sera forcé a 'resultat') :")
+
+    # Si nom de fichier vide force le nom 'resultat'
+    if choice_result_filename == "":
+        choice_result_filename = "resultat"
+
+    current_directory = getcwd()
+    result_filename_defined = current_directory + "\\" + choice_result_filename + ".csv"
+    return result_filename_defined
+
+def fill_result_file(file_path, line_to_fill):
+    # Fonction pour rajouter un ligne dans le fichier resultat
+    # Declaration variable
+    fileObj = Path(file_path)
+
+    en_tete = ["product_page_url",
+               "universal_ product_code",
+               "title",
+               "price_including_tax",
+               "price_excluding_tax",
+               "number_available",
+               "product_description",
+               "category",
+               "review_rating",
+               "image_url"]
+
+    # verifiation de l'existance du fichier si false je le crée et ajoute les en-tetes de colone
+
+    if not fileObj.exists():
+        with open(result_filename, 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file, delimiter=';')
+            writer.writerow(en_tete)
+
+    # si le fichier resultat.csv existe j'ajoute la ligne d'information pour un livre
+    if fileObj.exists():
+        with open(result_filename, 'a', newline='') as csv_file:
+            writer = csv.writer(csv_file, delimiter=';')
+            writer.writerow(line_to_fill)
+
+
+def get_choice_menu_UI():
+    execution_mode_code = ""
+    # Menu d'execution
+    print("###########################################################")
+    print("#                                                         #")
+    print("#               Scraping Book To Scrap                    #")
+    print("#                                                         #")
+    print("###########################################################")
+    print("\n")
+    print("1 : recuperation d'un livre.")
+    print("2 : recuperation d'une categorie.")
+    print("3 : recuperation du site complet.")
+    print("4 : Sortie sans execution.")
+
+    # Demande d'entrée du mode voulu : livre, category, complet
+    execution_mode_code = input("Entrez le chiffre du mode d'execution souhaité :")
+    return execution_mode_code
+
+
+def test_url(url):
+    # Test de la reponse de l'url
+    reponse_get_request = requests.get(url)
+    if reponse_get_request.status_code == 200:
+        url_to_test = url
+    else:
+        print("erreur sur l'url : " + str(reponse_get_request))
+        sys.exit()
+    return url_to_test
 
 # partie main du programme
 
 
-url = sys.argv[1]  # recuperation de l'url passé en argument dans le terminal
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+# intialisation variable
 
-current_directory = getcwd()
-result_filename = current_directory + "\\resultat.csv"
-fileObj = Path(result_filename)
 
-en_tete = ["product_page_url",
-           "universal_ product_code",
-           "title",
-           "price_including_tax",
-           "price_excluding_tax",
-           "number_available",
-           "product_description",
-           "category",
-           "review_rating",
-           "image_url"]
+execution_mode = ""
+url_OK = ""
+url_entry = ""
 
-# verifiation de l'existance du fichier si false je le crée et ajoute les en-tetes de colone
 
-if not fileObj.exists():
-    with open(result_filename, 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=';')
-        writer.writerow(en_tete)
+# Recuperation des argument passé au programme
 
-# si le fichier resultat.csv existe j'ajjoute la ligne d'information pour un livre
-if fileObj.exists():
-    line_to_write = get_data_book(url, soup)
-    
-    with open(result_filename, 'a', newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=';')
-        writer.writerow(line_to_write)
-    
+if len(sys.argv) > 1 and str(sys.argv[1]) == "livre":
+
+    # Partie livre
+    url_OK = test_url(sys.argv[2])
+    execution_mode = "1"
+
+elif len(sys.argv) > 1 and str(sys.argv[1]) == "categorie":
+
+    # Partie categorie
+    url_OK = test_url(sys.argv[2])
+    execution_mode = "2"
+
+elif len(sys.argv) > 1 and str(sys.argv[1]) == "complet":
+
+    # Partie site complet
+    url_OK = test_url(sys.argv[2])
+    execution_mode = "3"
+
+else:
+    # Sans argument passage en mode ui menu
+    execution_mode = get_choice_menu_UI()
+
+
+# Gestion du mode d'execution du programme
+
+# Gestion du choix
+if execution_mode == "1":
+
+    # pour un seul livre
+    result_filename = get_result_filename()
+    # Demande d'url si url null car provenant du menu
+    if url_OK == "":
+        url_entry = input("Entre l'url du livre a recuperer :")
+        url_OK = test_url(url_entry)
+
+    line_to_write = get_data_book(url_OK)
+    fill_result_file(result_filename, line_to_write)
+
+elif execution_mode == "2":
+
+    # pour une catégorie
+    result_filename = get_result_filename()
+    list_link_in_category = []
+    # Demande d'url si url null car provenant du menu
+    if url_OK == "":
+        url_entry = input("Entre l'url du livre a recuperer :")
+        url_OK = test_url(url_entry)
+
+    list_link_in_category = get_book_link_in_page(url_OK)
+    for link in list_link_in_category:
+        line_to_write = get_data_book(link)
+        fill_result_file(result_filename, line_to_write)
+
+elif execution_mode == "3":
+    # Pour le site entier
+    print(" site complet en construction")
+
+elif execution_mode == "4":
+    # sortie du programme
+    sys.exit()
+else:
+    print("entree incorrecte. Sortie du programme !" + str(execution_mode))
